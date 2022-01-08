18084d59597f1b71c6d001be50e5cd8ac81a51db
Zhaohang Chen <chenzh@cip.ifi.lmu.de>
2022-01-08 17:53:48 +0100

    damageCards effect

--- src/main/java/game/Card/Trojan.java
+++ src/main/java/game/Card/Trojan.java
@@ -1,8 +1,17 @@
 package game.Card;
 
+import game.Gamer;
+
+import java.util.concurrent.Callable;
+
 public class Trojan extends DamageCards{
     @Override
-    public void effect() {
-
+    public void effect(Gamer gamer) {
+      Cards card1 = spamCards.remove(0);
+      Cards card2 = spamCards.remove(0);
+      handCards.add(card1);
+      handCards.add(card2);
     }
+
+
 }
