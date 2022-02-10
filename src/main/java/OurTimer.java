e51a319e0be1eb88c5e1f62b04c2c9bd1ca7e1d5
Luca Weyhofen <weyhofen@cip.ifi.lmu.de>
2022-02-10 20:56:36 +0100

    Bugfix: - Timer after everyone has played his cards

--- src/main/java/OurTimer.java
+++ src/main/java/OurTimer.java
@@ -2,10 +2,22 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Diese Klasse stellt den Timer dar, der, sobald ein Spieler alle Register befüllt hat, 30 Sekunden dauert.
+ *
+ * @author Luca Weyhofen
+ *
+ * @version 2.1
+ */
 public class OurTimer {
 
     boolean necessary = true;
 
+    /**
+     * Dies ist der Konstruktor
+     * @param dauer Anzahl der Sekunden, die der Timer laufen soll
+     * @param game Das Spiel, bei dem der Timer abläuft
+     */
     public OurTimer(int dauer, Game game){
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         Runnable runnable = new Runnable() {
@@ -15,8 +27,10 @@ public class OurTimer {
                 countdown = countdown - 5;
                 if (countdown < 0 && necessary){
                     game.timerEnded();
+                    scheduler.shutdown();
+                } else if (countdown < 0) {
+                    scheduler.shutdown();
                 }
-                scheduler.shutdown();
             }
         };
         scheduler.scheduleAtFixedRate(runnable, 0, 5, TimeUnit.SECONDS);
@@ -24,9 +38,5 @@ public class OurTimer {
 
     public void setNecessary(boolean necessary) {
         this.necessary = necessary;
-    }
-
-    public boolean isNecessary() {
-        return necessary;
     }
 }
